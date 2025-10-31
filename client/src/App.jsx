d7e0ab47b067d986d4907e1de0123a193b82db27
Janice Morales-Melchor <S-Janice.Morales-Mel@lwtech.edu>
2025-10-30 17:32:00 -0700

    Implement code to App.jsx for storing a variable from the frotend.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,29 +5,66 @@ import axios from 'axios'
 function App() {
   const [count, setCount] = useState(0);
   const [array, setArray] = useState([]);
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    setList([...list, newTodo]);
+
+    setInput("");
+  };
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:3000/api");
     setArray(response.data.fruits);
     console.log(response.data.fruits);
   };
-
   useEffect(() => {
     fetchAPI();
   }, []);
 
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  }
+
   return (
+
+    <div>
+      <h1>Stored Recipes!</h1>
+      <input
+      type="text"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      /> 
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    /*
     <>
     
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        {/*
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        */}
         {
           array.map((fruit, index) => (
             <div key={index}>
@@ -38,7 +75,8 @@ function App() {
         }
       </div>
     </>
-  )
+    */
+  );
 }
 
 export default App
